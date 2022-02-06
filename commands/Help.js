cab248ad2a675b8e5490d57b91b466617285661b
P <pat.morin17@hotmail.com>
2022-02-06 16:09:31 -0500

    -summoner now working

--- commands/Help.js
+++ commands/Help.js
@@ -9,13 +9,13 @@ module.exports = {
             .setTitle('Here is all the currently available commands!')
             .addFields(
                 { name: "-music", value: "Enter this command to see the music-related commands!" },
+                { name: "-summoner [name]", value: 'Search for a league of legend account!' },
                 { name: "-coin", value: 'Flip a coin!' },
                 { name: "-number [min] [max]", value: "Want me to give you a number? Type this command followed by your desired interval (ex: -number 2 4). If you do not give me an interval, I'll give you a number between 1 and 10! ^^'" },
                 { name: "-headpat", value: "Show a random (but handpicked) headpat picture" },
                 { name: "-video", value: "The best content you can get." },
-                { name: "-poll description, option1, option2, ...", value: "Create a poll. Be carefull with the syntax, I need the commas. If you do not give options, it will be a yes no poll." },
+                { name: "-poll [description], [option1], [option2], ...", value: "Create a poll. Be carefull with the syntax, I need the commas. If you do not give options, it will be a yes no poll." },
                 { name: "-pollFace description", value: "Create a poll, but with beautiful faces. owo" },
-                { name: "-summoner [name]", value: 'Work in progress - Search for a league of legend account!' },
                 { name: "Question of the Day!", value: "I will send a question everyday at 12:00!" },
                 { name: "Various references", value: "Try and find them!" },
                 { name: "Cool reactions", value: "I'll automatically react if I read something special." },
